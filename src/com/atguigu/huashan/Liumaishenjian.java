f2ab6870995ee2b2ddd0ad4b744286298260c2a6
yushuaiqi <1036345741@qq.com>
2017-07-28 18:44:44 +0800

    第一次提交一本剑谱！

---
+++ src/com/atguigu/huashan/Liumaishenjian.java
@@ -0,0 +1,10 @@
+package com.atguigu.huashan;
+
+public class Liumaishenjian {
+	
+	public static void main(String[] args) {
+		System.out.println("我是一个剑客");
+		System.out.println("我是一个剑客");
+	}
+
+}
