12751bb709df01b0eba389dab941c534dc78db8c
yushuaiqi <1036345741@qq.com>
2017-07-28 19:27:00 +0800

    证明自己是一个剑客

--- src/com/atguigu/huashan/Liumaishenjian.java
+++ src/com/atguigu/huashan/Liumaishenjian.java
@@ -5,6 +5,8 @@ public class Liumaishenjian {
 	public static void main(String[] args) {
 		System.out.println("我是一个剑客");
 		System.out.println("我是一个剑客");
+		
+		System.out.println("我真的是一个剑客！");
 	}
 
 }
